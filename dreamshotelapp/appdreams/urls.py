027e4d7236b3ff1c1f6cf86dbd1393115e85fcc7
ahmadmalbzoor <ahmadmalbzoor@gmail.com>
2021-06-03 21:30:13 +0300

    voucher and complete your booking file

--- dreamshotelapp/appdreams/urls.py
+++ dreamshotelapp/appdreams/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path("cards",views.cards),
+    path('voucher',views.voucher)
 ]
--- dreamshotelapp/appdreams/views.py
+++ dreamshotelapp/appdreams/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render, HttpResponse
 def index(request):
     return HttpResponse("this is the equivalent of @app.route('/')!")
+def cards(request):
+    return render(request,"checkout.html")
+
+def voucher(request):
+    return render(request,"voucher.html")
